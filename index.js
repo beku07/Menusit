e96ada41b2c06aa9b7cc514a59ae761ef933d616
beku kumushbelov <beku200418gmail.com>
2023-10-02 14:52:51 +0600

    first com

---
+++ index.js
@@ -0,0 +1,18 @@
+    // // Получаем элемент с классом "floating-text"
+    // var floatingText = document.querySelector('.floating-text');
+
+    // // При наведении курсора мыши на элемент "floating-text"
+    // floatingText.addEventListener('mouseover', function() {
+    //   // Остановить анимацию
+    //   floatingText.style.animationPlayState = 'paused';
+    // });
+
+    // // При уходе курсора мыши с элемента "floating-text"
+    // floatingText.addEventListener('mouseout', function() {
+    //   // Возобновить анимацию
+    //   floatingText.style.animationPlayState = 'running';
+    // });
+
+    function redirectToOtherSite() {
+        window.location.href = "https://restoran.kg/restaurant/1625-shtaynbroy";
+      }
